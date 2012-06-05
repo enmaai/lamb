e3dfcab34d4b1e686e01a21edb0b50d15f0e0872
enmaai <enmaai>
2012-06-05 14:12:39 +0800

    initial

--- assets/js/lamb.js
+++ assets/js/lamb.js
@@ -1,5 +1,6 @@
 (function(){
-    var cache = {};
+    var cache = {},
+        _current;
     //get a dom node
     function _get(selector){
         if(!cache[selector]){
@@ -25,23 +26,56 @@
                 opacity : 1
             },3000); 
         },1000);
-        _get('.sub-title').tap(function(){
+        _get('.sub-title').click(function(){
             menu();
         });
     }
 
+    function showAndHide(theShow,theHide){
+        _get(theShow).show();
+        _get(theHide).hide();
+        _current = theShow;
+    }
+
     function menu(){
-        _get('#welcome').hide();
-        _get('#menu').show();
-        _get('.start').tap(function(){
+        showAndHide('#menu','#welcome');
+        _get('.main').show();
+        _get('.start').click(function(){
             act1();
         });
     }
 
     function act1(){
-        _get('#menu').hide();
-        _get('#act1').show();
+        showAndHide('#act1','#menu');
+        setTimeout(function(){
+            act2();
+        },1000);
     }
+
+    function act2(){
+        renderSheep();
+        showAndHide('#act2','#act1');
+    }
+
+    function renderSheep(){
+        var w = _get(window).width()-50,
+            h = _get(window).height()-50,
+            str = '';
+        for(var i=0;i<10;i++){
+            var tmp = randomPosition(w,h);
+            str += '<div class="sheep" id="sheep' + i + '" style="left:'+tmp[0]+'px;top:'+tmp[1]+'px"></div>';
+        }
+        _get('#act2').html(str);
+    }
+
+    function randomPosition(w,h){
+        return [Math.random()*w,Math.random()*h];
+    }
+
+    _get('.main').click(function(e){
+        _get(this).hide();
+        showAndHide('#welcome',_current);
+    });
 
     welcome();
 })()
